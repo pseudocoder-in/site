d597f4db89180f6aaa9613082db21a836c67f62a
Subhash Jha <subhashjha.mail@gmail.com>
2022-07-31 00:28:43 +0530

    adding projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,30 @@ const projectsData = [
     imgSrc: '/static/images/tool_enhancer.png',
     href: 'https://exchange.adobe.com/creativecloud/plugindetails.html/app/cc/196be244?pluginId=196be244&workflow=share',
   },
+  {
+    title: 'Draw ++',
+    description: `Draw using keyboard and mouse.`,
+    imgSrc: '/static/images/draw.png',
+    href: 'https://subhashjha.in/draw/',
+  },
+  {
+    title: 'Camera Controller',
+    description: `Photoshop plugin to view layers in 3d.`,
+    imgSrc: '/static/images/camera.png',
+    href: 'https://adobe.com/go/cc_plugins_discover_plugin?pluginId=88abba36&workflow=share',
+  },
+  {
+    title: 'Family Tree App ',
+    description: `Webapp to create family tree with ease`,
+    imgSrc: '/static/images/tree.png',
+    href: 'https://tree.pseudocoder.in/',
+  },
+  {
+    title: 'Rent Receipt Generator ',
+    description: `Generate rent receipts for ITR proof submission.`,
+    imgSrc: '/static/images/rent.png',
+    href: 'https://subhashjha.in/rentreceipt/',
+  },
 ]
 
 export default projectsData
--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home({ posts }) {
               <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
               <Link href="https://subhashjha.in/">
                 <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
-                  <span className="flex items-center space-x-5">
+                  <span className="flex items-center space-x-4">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-6 w-6 -rotate-6 text-pink-600"
@@ -89,7 +89,7 @@ export default function Home({ posts }) {
                         d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                       />
                     </svg>
-                    <span className="pr-6 text-gray-900 dark:text-gray-100">Read my story</span>
+                    <span className="pr-6 text-gray-900 dark:text-gray-100">More about me</span>
                   </span>
                   <span className="pl-6 text-indigo-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
                     Website&nbsp;&rarr;
